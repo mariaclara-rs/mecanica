6f348aacd0adaaea69478983bfa717f9c28926d2
mariaclara-rs <mariaclara.rgs@gmail.com>
2022-09-18 10:34:28 -0300

    *****

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,16 +4,22 @@ const cors = require('cors')
 const app = express();
 require('dotenv').config()
 
+const corsOpts = {
+    origin: '*',
+  
+    methods: [
+      'GET',
+      'POST',
+      'PUT',
+      'DELETE'
+    ],
+  
+    allowedHeaders: [
+      'Content-Type',
+    ],
+  };
 
-app.use(
-    function(req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-        res.setHeader('Access-Control-Allow-Credentials', true);
-        next();
-    }
-);
+app.use(cors(corsOpts));
 app.use(express.json());
 app.use(routes);
 //use indica que deve ser aplicado para todas as rotas
