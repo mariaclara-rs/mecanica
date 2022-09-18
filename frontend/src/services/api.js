7ba5dd4c7b3bee708af75c987a70bfc49d0f8102
mariaclara-rs <mariaclara.rgs@gmail.com>
2022-09-18 11:14:58 -0300

    -

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -5,7 +5,9 @@ const api  = axios.create({
     baseURL:'https://mecanicaapi.vercel.app',
     headers: {
       'Authorization': token(),
-      'Access-Control-Allow-Origin': "*"
+      'Access-Control-Allow-Origin': "*",
+      "Access-Control-Allow-Headers" : "Content-Type",
+      "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PUT,DELETE"
     }
 });
 
